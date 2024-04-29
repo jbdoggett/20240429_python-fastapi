b51874bfeffafb5778977cf4dfff9cbfd805de9d
jbdoggett <37301977+jbdoggett@users.noreply.github.com>
2024-04-29 20:27:21 +0000

    db and crud

---
+++ demos/colors_api/crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+
+import models_w_db
+import schemas
+
+def get_colors(db: Session) -> list[models_w_db.Color]:
+    return db.query(models_w_db.Color).all()
+
+def create_color(db: Session, color: schemas.Color) -> models_w_db.Color:
+    db_color = models_w_db.Color(
+        name=color.name, hex_code=color.hex_code)
+    db.add(db_color)
+    db.commit()
+    db.refresh(db_color)
+    return db_color
